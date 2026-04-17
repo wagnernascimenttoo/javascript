50b414b89aeda4a6c6bd6ad6b28aa3841760f119
Wagner Nascimento <wagnernascimenttoo@outlook.com>
2026-04-17 15:26:25 -0300

    criando novo exercicio

---
+++ exercicios/ex004/ex001.js
@@ -0,0 +1,5 @@
+var vel = 88
+if (vel > 60.5) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')}
+console.log(`A velocidade do seu carro e ${vel} km/h`)
+console.log('Dirija sempre usando cinto de seguranca!')
---
+++ exercicios/ex004/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+    console.log (`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log ('Voce e Brasileiro')
+} else {
+    console.log('Voce e Estrangeiro!')
+}
